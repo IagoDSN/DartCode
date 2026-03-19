84cb5e35a56d06a6280de4643d32e49ac65d763a
Iago Nunes <iago.diniz.nunes@gmail.com>
2026-03-19 09:02:39 -0300

    atividade

---
+++ codigos/att1.dart
@@ -0,0 +1,29 @@
+import 'dart:io';
+import 'dart:math';
+
+void main(){
+  /*
+  print("Digite um valor:");
+  int valor1 = stdin.readLineSync()!;
+
+  print("Digite outro valor:");
+  int valor2 = stdin.readLineSync()!;
+
+  print("A soma dos valores é: ${valor1 + valor2}");
+  */
+
+  /*
+  print("Digite um valor:");
+  var valoratt2 = double.parse(stdin.readLineSync()!);
+  print("o valor digitado foi: $valoratt2, o antecessor é ${valoratt2 - 1} e o sucessor é ${valoratt2 + 1}");
+  */
+
+  /*
+  print("Digite um valor:");
+  var valoratt3 = double.parse(stdin.readLineSync()!);
+  print("digite outro valor:");
+  var valoratt4 = double.parse(stdin.readLineSync()!);
+  double media = (valoratt3 + valoratt4) / 2;
+  print("A média dos valores é: $media");
+  */
+}
