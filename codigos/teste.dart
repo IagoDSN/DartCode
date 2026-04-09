2e478bdaf26237f6866c85bfdf9005d385610bb2
Iago Nunes <iago.diniz.nunes@gmail.com>
2026-04-09 10:29:35 -0300

    t

--- codigos/teste.dart
+++ codigos/teste.dart
@@ -1,6 +1,5 @@
 import 'dart:io';
   void main() {
-  /*18 – Construa um programa que receba um número inteiro e verifique se o mesmo é primo. Um número é classificado como primo se ele é maior do que um e é divisível apenas por um e por ele mesmo. Exemplo de números primos: 2,3,5,7,11,13.*/
   print("Digite um número inteiro:");
   int numero = int.parse(stdin.readLineSync()!);
   bool primo = true;
