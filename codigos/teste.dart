dfebb59a4b1cd9225803ce7c6f74a324dbd686f1
Iago Nunes <iago.diniz.nunes@gmail.com>
2026-05-14 10:01:33 -0300

    main

--- codigos/teste.dart
+++ codigos/teste.dart
@@ -1,21 +1,16 @@
 import 'dart:io';
-  void main() {
-  print("Digite um número inteiro:");
-  int numero = int.parse(stdin.readLineSync()!);
-  bool primo = true;
-  if (numero <= 1) {
-    primo = false;
-  } else {
-    for (int i = 2; i <= numero ~/ 2; i++) {
-      if (numero % i == 0) {
-        primo = false;
-        break;
-      }
-    }
-  }
-  if (primo) {
-    print("$numero é um número primo.");
-  } else {
-    print("$numero não é um número primo.");
-  }
+int contarPalavras(String frase) {
+  List palavras =
+      frase.split(" ");
+  return palavras.length;
+}
+void main() {
+  print("Digite uma frase:");
+  String frase =
+      stdin.readLineSync()!;
+  int quantidade =
+      contarPalavras(frase);
+  print(
+    "Quantidade de palavras: $quantidade"
+  );
 }
